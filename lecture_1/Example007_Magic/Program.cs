45b34cc1f3cd5eb6dca77c0c9c70c0d924b59793
suntsovpavel <suntsovpavel@mail.ru>
2023-01-14 23:59:57 +0300

    add some projects lecture 1

---
+++ lecture_1/Example007_Magic/Program.cs
@@ -0,0 +1,29 @@
+﻿System.Console.Clear();
+
+int xa = 40, ya = 1, xb = 1, yb = 20, xc = 80, yc = yb;
+
+System.Console.SetCursorPosition(xa, ya);
+System.Console.WriteLine("+");
+System.Console.SetCursorPosition(xb, yb);
+System.Console.WriteLine("+");
+System.Console.SetCursorPosition(xc, yc);
+System.Console.WriteLine("+");
+
+int x=xa, y=ya;
+int cnt = 0;
+while(cnt < 10000){
+    int what = new Random().Next(0,3);  //0 1 2
+    if(what == 0){
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }else if(what == 1){
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }else{      
+        x = (x+xc)/2;
+        y = (y+yc)/2;       
+    }
+    System.Console.SetCursorPosition(x, y);
+    System.Console.WriteLine("+");
+    cnt++;
+}
