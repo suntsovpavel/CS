0c33c85ddc8ff46ff3b4781020827e8f11573567
suntsovpavel <suntsovpavel@mail.ru>
2023-01-15 06:48:22 +0300

    add task3 from homework practical 1

---
+++ practical_1/homework/task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число и выдает, является ли число четным (делится на 2 ьез остатка)
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+
+if(a % 2 == 0){
+    System.Console.WriteLine($"Число {a}: четное");   
+}else{
+    System.Console.WriteLine($"Число {a}: нечетное");   
+}
