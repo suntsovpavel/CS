fcd0d0d99be1c3ca9bf6d8663a53f7ec498fd436
suntsovpavel <suntsovpavel@mail.ru>
2023-01-15 06:58:41 +0300

    add task4 from homework practical 1

---
+++ practical_1/homework/task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N, а на выходе показывает все четные числа от 1 до N
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N < 1){
+    System.Console.WriteLine($"Введенное число {N} меньше единицы");
+}else{
+    int i = 1;
+    System.Console.Write($"Четные числа от 1 до {N}: ");
+    while(i <= N){
+        if(i % 2 == 0){
+            System.Console.Write($"{i} ");   
+        }    
+        i++;
+    }
+}
+
+
