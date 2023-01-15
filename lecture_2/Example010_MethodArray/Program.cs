d09f14876b870eeccc458fae70b504b313987f90
suntsovpavel <suntsovpavel@mail.ru>
2023-01-16 02:25:52 +0300

    add Example010_MethodArray in lecture 2

---
+++ lecture_2/Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 12, 32, 43, 3, 34, 76, 56, 12, 62 };
+
+int n = array.Length;
+
+//Число, которое будем искать в массиве array
+int find = 34;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Индекс искомого элемента: {index}");
+        break;  //прерывание цикла
+    }
+
+    index++;
+}
