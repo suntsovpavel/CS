e41827b1cf79758717734fe57e21e6ca5b657a5c
suntsovpavel <suntsovpavel@mail.ru>
2023-01-16 02:57:19 +0300

    add Example11_ArrayLibrary in lecture 2

---
+++ lecture_2/Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int i = 0;
+    while (i < length)
+    {
+        collection[i] = new Random().Next(1, 10);    //по идее 1 и 10 нужно втащить в аргументы...
+        i++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int j = 0;
+    while (j < count)
+    {
+        Console.WriteLine(col[j]);
+        j++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int len = collection.Length;
+    int i = 0;
+    int find_pos = -1;
+    while (i < len)
+    {
+        if(collection[i] == find){
+            find_pos = i;
+            break;    //если не поставить этот break, будем дальше идти по циклу, и в итоге будет возвращен индекс не первого, а последнего вхождения    
+        }    
+        i++;
+    }
+    return find_pos;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
