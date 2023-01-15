9ba45f6befad442f55679f4da47ed55e0a4d208e
suntsovpavel <suntsovpavel@mail.ru>
2023-01-16 01:57:38 +0300

    add Example008 introMethod in lecture 2

---
+++ lecture_2/Example008_IntroMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int a1 = 12, b1 = 32, c1 = 43,
+    a2 = 3, b2 = 34, c2 = 76,
+    a3 = 56, b3 = 12, c3 = 62;
+
+// Эта функция возвращает максимальное из 3-х чисел
+int Max(int arg1, int arg2, int arg3)
+{
+    int res = arg1;
+    if (arg2 > res) res = arg2;
+    if (arg3 > res) res = arg3;
+    return res;
+}
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+Console.WriteLine(max);
