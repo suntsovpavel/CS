550f1a5707b1e2c361dc77a56553e2d443572518
suntsovpavel <suntsovpavel@mail.ru>
2023-01-18 00:32:49 +0300

    небольшая допилка task_4 в части использования Random

--- practical_2/homework/task_4/Program.cs
+++ practical_2/homework/task_4/Program.cs
@@ -1,15 +1,9 @@
 ﻿//Задача 4 * : Напишите программу, которая выводит случайное трёхзначное число 
 //и удаляет вторую цифру этого числа. Не использовать строки для расчета.
-System.Console.Write("Введите трехзначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+//System.Console.Write("Введите трехзначное число: ");
+//int a = Convert.ToInt32(Console.ReadLine());
 
-if (a > 99 && a < 1000)
-{
-    int result = (a / 100) * 10 + (a % 10);
-    System.Console.WriteLine($"Число {a} с удаленной второй цифрой: {result}");
-}
-else
-{
-    System.Console.WriteLine($"Число {a}: не трехзначное!");
-}
+int a = new Random().Next(100, 1000);
 
+int result = (a / 100) * 10 + (a % 10);
+System.Console.WriteLine($"Число {a} с удаленной второй цифрой: {result}");
