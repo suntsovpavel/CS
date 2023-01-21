2b0fa88197c18889d8c3d7ba9fe77451d26f4996
suntsovpavel <suntsovpavel@mail.ru>
2023-01-22 02:33:02 +0300

    add task_3 from homework practice 3

---
+++ practical_3/homework/task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+int PromptInt(string mess)
+{
+    System.Console.Write($"{mess} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = PromptInt("Введите целое число больше единицы");
+if(n < 1){
+    System.Console.WriteLine($"Введенное число {n} меньше единицы!");
+    return;
+}
+
+int i = 1;
+System.Console.Write($"Ряд кубов целых чисел от 1 до {n}:  ");
+while (i <= n)
+{
+    System.Console.Write(Math.Pow(i, 3));
+    if(i < n){
+        System.Console.Write(", ");
+    }
+    i++;
+}
+
