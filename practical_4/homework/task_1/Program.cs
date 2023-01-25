b8abde2721ffa948623fa469160a7f8537b6f17c
suntsovpavel <suntsovpavel@mail.ru>
2023-01-25 07:48:15 +0300

    add task_1, homework practical 4

---
+++ practical_4/homework/task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//Ввод целого значения 
+int PromptInt(string mess)
+{
+    System.Console.Write($"{mess} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+//Возвращаем num в степени exp
+int MyExponent(int num, int exp)
+{
+    int result = num;
+    while(exp > 1){
+        result *= num;
+        exp--;    
+    }
+    return result;
+}
+
+
+int n = PromptInt("Введите число (A)");
+int exponent = PromptInt("Введите показатель степени (B)");
+
+if(exponent < 1){
+    System.Console.WriteLine($"Введен некорректный показатель степени {exponent}");
+}else{
+    System.Console.WriteLine($"{n} в степени {exponent} равно {MyExponent(n, exponent)}");
+}
