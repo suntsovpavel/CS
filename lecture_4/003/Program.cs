dc2fe635e62bba65bb387c33270126f3daa59623
suntsovpavel <suntsovpavel@mail.ru>
2023-02-03 00:15:53 +0300

    add lecture_4

---
+++ lecture_4/003/Program.cs
@@ -0,0 +1,27 @@
+﻿int Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+//Console.WriteLine(Factorial(3));        // 1 * 2 * 3 = 6
+
+/*//Демонстрация переполнения типа (int):
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}*/
+
+//Для срвнения для типа double:
+double FactorialDbl(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * FactorialDbl(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {FactorialDbl(i)}");
+}
---
+++ lecture_4/004_Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}*/
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 43; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
