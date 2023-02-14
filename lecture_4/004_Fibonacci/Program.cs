d03b9d9eaf0e7939d9b2a28cbac9d9ad39557b83
suntsovpavel <suntsovpavel@mail.ru>
2023-02-14 08:31:14 +0300

    correct Fibonacci

--- lecture_4/004_Fibonacci/Program.cs
+++ lecture_4/004_Fibonacci/Program.cs
@@ -9,7 +9,7 @@ for (int i = 1; i < 10; i++)
     Console.WriteLine(Fibonacci(i));
 }*/
 
-double Fibonacci(int n)
+/*double Fibonacci(int n)
 {
     if (n == 1 || n == 2) return 1;
     else return Fibonacci(n - 1) + Fibonacci(n - 2);
@@ -17,5 +17,21 @@ double Fibonacci(int n)
 for (int i = 1; i < 43; i++)
 {
     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}*/
+
+// Используем массив хранения результатов
+double[] array = new double[1000];
+double Fibonacci(int n)  //целое от 1 и более
+{
+    bool withinBorders = n > 0 && n <= array.Length;
+    if (withinBorders && array[n - 1] > 0) return array[n - 1];
+    double result = (n < 3) ? 1 : Fibonacci(n - 1) + Fibonacci(n - 2);
+    if (withinBorders) array[n - 1] = result;
+    return result;
+}
+
+for (int i = 1; i < 1000; i++)
+{
+    System.Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}");
 }
 
