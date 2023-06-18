968c188f410484ded7eeaba16b9712b0cfaf756b
suntsovpavel <suntsovpavel@mail.ru>
2023-06-18 19:09:46 +0300

    edit exampleClass/

--- exampleClass/Program.cs
+++ exampleClass/Program.cs
@@ -8,26 +8,26 @@ class Program {
 
     public class Figure {
         Point[]? data = null;
-        public Figure(int size=0){ 
-            if(size > 0) data = new Point [size]; 
-        }
-        public Figure(int size, Point[] points){
-            if(size > 0) data = new Point [size]; 
-            setData(points);
-        }
 
-        public void setData(Point[] points){            
-            if (data == null)data = new Point [points.Length]; 
+        public bool isInit(){ return data != null; }
 
-            if(points.Length != data.Length) Console.WriteLine($"!!! size Point[] points not match size data,={points.Length},{data.Length}");
-            
+        public Figure(){}
+        public Figure(Point[] points){
+            init(points);
+        }
+        public void init(Point[] points){            
+            if (isInit()){
+                Console.WriteLine("!!! warning, object already initialized");    
+                return; 
+            }
+            data = new Point[points.Length];                      
             for(int i=0; i<points.Length; i++){
                 data[i] = points[i];
             }    
         }
 
         public void Print(){
-            if (data == null) return;
+            if (!isInit()) return;
             for(int i=0; i<data.Length; i++){
                 Console.Write($"({data[i].x},{data[i].y})");    
                 if(i<data.Length-1) Console.Write(",");
@@ -38,15 +38,16 @@ class Program {
 
     public static void Main (string[] args) {
         //а) треугольник
-        Figure triangle = new Figure(3, new Point[] {new Point(0,0), new Point(0,1), new Point(1,0)});            
+        Figure triangle = new Figure(new Point[] {new Point(0,0), new Point(0,1), new Point(1,0)});            
         Console.Write("triangle: "); triangle.Print();
 
-        //с отложенной инициализацией списком узлов:
-        Figure triangle2 = new Figure(3);            
-        triangle2.setData(new Point[] {new Point(0,0), new Point(0,1), new Point(1,0)});
+        //б) треугольник с отложенной инициализацией списком узлов:
+        Figure triangle2 = new Figure();            
+        triangle2.init(new Point[] {new Point(0,0), new Point(0,1), new Point(1,0)});
         Console.Write("second triangle: "); triangle2.Print();   
-        //пентагон
-        Figure pentagon =  new Figure(5, new Point[] {new Point(0,0), new Point(0,1), new Point(1,0), new Point(-1,0), new Point(0,-1)});            
+        
+        //в) пентагон
+        Figure pentagon =  new Figure(new Point[] {new Point(0,0), new Point(0,1), new Point(1,0), new Point(-1,0), new Point(0,-1)});            
         Console.Write("pentagon: "); pentagon.Print();
     }
 }
